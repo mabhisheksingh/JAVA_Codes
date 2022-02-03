1cf8803c801d2717df68a9007161e94ed8b4efe6
Abhishek Singh <87180842+mabhisheksingh@users.noreply.github.com>
2022-02-03 15:35:52 +0530

    ReverseWordsInAString java code added

---
+++ src/main/java/leetcode/medium/ReverseWordsInAString.java
@@ -0,0 +1,21 @@
+package leetcode.medium;
+
+public class ReverseWordsInAString {
+/*
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+*/
+    public static void main(String[] args) {
+        String s = "    the   sky is blue  ";
+        System.out.println("reverse a word:"+reverseWords(s)+":");
+    }
+    private static String reverseWords(String s) {
+        String output = "";
+        String str[] = s.split(" ");
+        for (int i = str.length-1; i >=0; i--) {
+            output=output.trim()+" "+str[i].trim();
+        }
+        System.out.println("output : "+output);
+        return output.trim();
+    }
+}
